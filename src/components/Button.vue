42da7e2f370f65ebdebda25c06779b1311db8566
wintertime7 <sgwa2001@gmail.com>
2022-11-09 23:06:46 +0200

    header fully ready

---
+++ src/components/Button.vue
@@ -0,0 +1,62 @@
+<script>
+  export default {
+    name: "Button",
+    data() {
+      return {
+        text: "LOGIN"
+      }
+    },
+    methods: {
+      toggleText() {
+        if (this.$parent.show == false) {
+          this.text = "LOGOUT";
+          this.$parent.show = true;
+        } else {
+          this.text = "LOGIN";
+          this.$parent.show = false;
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <button @click="toggleText" v-bind:class="{'log-btn': this.$parent.show == false, 'log-btn-clicked': this.$parent.show}">{{text}}</button>
+</template>
+
+<style>
+  .log-btn {
+    background-color: var(--purple-color);
+    padding: 5px 16px;
+    border: none;
+    border-radius: 50px;
+    cursor: pointer;
+    transition: all 0.3s ease 0s;
+    color: var(--white-color);
+    font-family: 'Poppins', sans-serif;
+    font-weight: 700;
+    margin-right: 40px;
+  }
+
+  .log-btn:hover {
+    background-color: var(--purple-hover);
+  }
+
+  .log-btn-clicked {
+    background-color: rgb(9, 9, 9);
+    padding: 5px 16px;
+    border: none;
+    border-radius: 50px;
+    cursor: pointer;
+    transition: all 0.3s ease 0s;
+    color: var(--white-color);
+    font-family: 'Poppins', sans-serif;
+    font-weight: 700;
+    margin-right: 40px;
+  }
+
+  .log-btn-clicked:hover {
+    background-color: var(--purple-hover);
+  }
+
+</style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,16 +1,45 @@
 <script>
-  export default {
+  import Button from './Button.vue';
 
+  export default {
+    name: 'HeaderComponent',
+    components: {
+      Button
+    },
+    data() {
+      return {
+        //pls change this after testing u idiot
+        show: false,
+        user: {
+          name: "Rainers",
+          surname: "Smolenskis",
+          code: "IT20008"
+        }
+      }
+    },
+    methods: {
+      getFullName(name, surname) {
+        let full_name = name + " " + surname;
+        return full_name;
+      }
+    }
   }
+
+  
+
 </script>
 
 <template>
-  <div class="nav-bar">
+  <div v-bind:class="{'nav-bar': this.show == false, 'nav-bar-active': this.show}">
     <div class="nav-logo">
       <img src="../assets/logo-white.svg">
       <h1>KRAKEN.FM</h1>
     </div>
-    <button class="log-btn">LOGIN</button>
+    <div v-if="show" class="account">
+      <span class="dot"></span>
+      <p class="user-name">{{getFullName(user.name, user.surname)}}</p>
+    </div>
+    <Button />
   </div>
 </template>
 
@@ -18,13 +47,23 @@
   .nav-bar {
     background-color: var(--gray-color);
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-end;
+    align-items: center;
+    height: 70px;
+    border-bottom: 2px var(--purple-color) solid;
+  }
+
+  .nav-bar-active {
+    background-color: var(--purple-color);
+    display: flex;
+    justify-content: flex-end;
     align-items: center;
     height: 70px;
     border-bottom: 2px var(--purple-color) solid;
   }
 
   .nav-logo {
+    margin-right: auto;
     display: flex;
     align-items: center;
     margin-left: 40px;
@@ -42,21 +81,30 @@
     cursor: pointer;
   }
 
-  .log-btn {
-    background-color: var(--purple-color);
-    padding: 5px 16px;
-    border: none;
-    border-radius: 50px;
-    cursor: pointer;
-    transition: all 0.3s ease 0s;
-    color: var(--white-color);
-    font-family: 'Poppins', sans-serif;
-    font-weight: 700;
-    margin-right: 40px;
+  .account {
+    height: 35px;
+    display: flex;
+    align-items: center;
+    margin-right: 7px;
+    border-right: 2px white solid;
   }
 
-  .log-btn:hover {
-    background-color: var(--purple-hover);
+  .user-name {
+    color: var(--white-color);
+    font-family: 'Poppins', sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    margin-right: 7px;
   }
+
+.dot {
+  height: 33px;
+  width: 33px;
+  background-color: rgb(212, 212, 212);
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 7px;
+  border: 2px var(--white-color) solid;
+}
 
 </style>
